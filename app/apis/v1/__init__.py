e03ec05524175e6cacec33e99354375bb0715714
UrungE <klg23284@gmail.com>
2026-08-21 18:20:22 +0900

    [features/42][김진형]PR #46 소관인 import 정렬 변경 되돌림

--- app/apis/v1/__init__.py
+++ app/apis/v1/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
+from app.apis.v1.alarm_router import alarm_router
 from app.apis.v1.admin_auth_routers import admin_auth_router
 from app.apis.v1.admin_routers import admin_router
-from app.apis.v1.alarm_router import alarm_router
 from app.apis.v1.auth_routers import auth_router
 from app.apis.v1.job_router import job_router
 from app.apis.v1.user_routers import user_router
